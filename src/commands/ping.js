1290444322db39b26e22b59e69797a3d10e7b6ff
Dwsysfx <84414140+dwsysfx@users.noreply.github.com>
2023-03-15 22:02:10 -0500

    Ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,8 +5,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Check the ping of the bot and Discord api.',
 	async execute(message, args) {
-        console.log(message)
-        const sentMessage = await message.reply({ content: 'Pinging...', fetchReply: true });
 		const pingEmbed = new EmbedBuilder()
 			.setColor(0x32cd32)
 			.setTitle('Pong :ping_pong:')
@@ -14,12 +12,12 @@ module.exports = {
 			.setThumbnail('https://media.giphy.com/media/fvA1ieS8rEV8Y/giphy.gif')
 			.addFields(
 				{ name: 'Uptime', value: `${Math.round(client.uptime / 60000)}`, inline: true },
-				{ name: 'Bot Ping', value: `${sentMessage.createdTimestamp - message.createdTimestamp}ms`, inline: true },
+				{ name: 'Bot Ping', value: `${Date.now() - message.createdTimestamp}ms`, inline: true },
 				{ name: 'API Ping', value: `${client.ws.ping}ms`, inline: true },
 			)
 			.setFooter({ text: `${message.author.username}#${message.author.discriminator}`, iconURL: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}` })
 			.setTimestamp()
         
-            await message.edit('Hi')
+            await message.channel.send({ embeds: [pingEmbed] })
 	},
 };
