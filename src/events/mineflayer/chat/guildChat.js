f6f041369a62f8f9391d3989983f99fd76b393de
Dwsysfx <84414140+dwsysfx@users.noreply.github.com>
2023-03-18 22:51:31 -0500

    Made guild to discord work
    Omg finally got it to work, how exciting!

--- src/events/mineflayer/chat/guildChat.js
+++ src/events/mineflayer/chat/guildChat.js
@@ -1,9 +1,18 @@
-const mineflayer = require('mineflayer')
-const { bot } = require('../../../handler/bot')
+const { EmbedBuilder } = require('discord.js');
+const util = require('../../../util/message');
 
 module.exports = {
     name: 'chat:guildChat',
 	async execute(channel, rank, name, grank, msg) {
-        console.log(`${channel}     ${rank}     ${name}      ${grank}       ${msg}`)
+        let guildRank = '';
+        if (typeof grank === 'undefined'){ 
+            guildRank = '';
+        } else { guildRank = grank }
+        let rankDetails = util.getRank(rank);
+        const embed = new EmbedBuilder()
+            .setDescription(`${rankDetails[0] ? rankDetails[0] : ""}` + ` ${name}` + `${grank ? " " + grank + ":"  : ":"}` + ` ${msg}`)
+            .setColor(rankDetails[1])
+
+        util.sendToDiscord(channel, embed)
 	},
 };
