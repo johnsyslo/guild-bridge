85bc179e877e719aa4d79464ad6097c9967af406
Dwsysfx <84414140+dwsysfx@users.noreply.github.com>
2023-03-19 20:35:54 -0500

    Added join leave message

--- src/events/mineflayer/chat/joinLeave.js
+++ src/events/mineflayer/chat/joinLeave.js
@@ -1,6 +1,14 @@
+const { EmbedBuilder } = require('discord.js')
+const util = require('../../../util/message')
+
 module.exports = {
     name: 'chat:joinLeave',
 	async execute(player, status) {
-        console.log(`${player} has ${status}!`)
+        let emoji = '';
+        if (status === 'joined'){ emoji = `:inbox_tray:` } else { emoji = `:outbox_tray:` }
+        const embed = new EmbedBuilder()
+            .setDescription( `${emoji} ${player} has ${status}!`)
+
+        util.sendToDiscord('guild', embed);
 	},
 };
